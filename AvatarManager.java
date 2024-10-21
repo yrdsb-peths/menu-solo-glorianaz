25d2bad08893beac515e3beecff73e6ebf1ec62b
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-10-21 12:27:24 -0400

    Managing the Avatar's
    Make it so the player is abel to click through the avatar (using queues)

--- AvatarManager.java
+++ AvatarManager.java
@@ -3,15 +3,53 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class AvatarManager here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Gloriana Zheng
+ * @version October 2024
  */
 public class AvatarManager extends Actor
 {
-    /**
-     * Act - do whatever the AvatarManager wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    // Create Queue of all the avatars 
+    private Queue<Avatar> avatars = new Queue<>();
+
+    // current avatar 
+    private Avatar current;
+
+    public AvatarManger()
+    {
+        setImage(getImage() null);
+        avatars.enqueue(new Avatar("mario.png"));
+        avatars.enqueue(new Avatar("luigi.png"));
+        avatars.enqueue(new Avatar("bowser.png"));
+    
+        current = avatars.dequeue();
+
+    }
+
+    protected void addedToWorld(World world)
+    {
+        world.addObject(new Button(() -> nextAvatar()), world.getWidth() / 2, world.getHeight() / 2);
+        updateAvatar();
+    }
+
+    public void updateAvatar()
+    {
+        int x = 300;
+        int y = 100;
+
+        if(current != null)
+        {
+            getWorld().removeObject(current);
+        }
+        world.addObject(current, x, y);
+    }
+
+    public void nextAvatar()
+    {
+        avatars.enqueue(current);
+        getWorld().removeObject(current);
+        current = avatars.dequeue;
+        updateAvatar();
+    }
     public void act()
     {
         // Add your action code here.
