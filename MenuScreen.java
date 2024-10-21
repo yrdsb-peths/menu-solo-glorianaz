6467a4f91107517cc0514117fa8aa3cab1a3f02c
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-10-21 17:37:27 -0400

    Converted Images to PNG

--- MenuScreen.java
+++ MenuScreen.java
@@ -8,20 +8,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MenuScreen extends World
 {
-    public MenuScreen() {
+    public MenuScreen() 
+    {
         super(600, 400, 1);
 
-        //addObject(new Button(this::goInstructions), 300, 340);
         addObject(new Button(() -> Greenfoot.setWorld(new InstructionScreen(this))), 300, 340);
+        
+        AvatarManager avatarManager = new AvatarManager();
+        addObject(avatarManager,300, 100);
     }
 
-    public void goInstructions() {
-        Greenfoot.setWorld(new InstructionScreen(this));
-    }
-
-    
-    AvatarManager avatarManager = new AvatarManager();
-    // addObject(avatarManager,300, 100);
+   
 }
 
 
