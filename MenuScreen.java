09c319bea405786fd72ad70a602e23f13af78cf9
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-10-21 19:30:13 -0400

    Choose character Label

--- MenuScreen.java
+++ MenuScreen.java
@@ -3,11 +3,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MenuScreen here.
  * 
- * @author Zheng
+ * @author Gloriana Zheng
  * @version Oct 2024
  */
 public class MenuScreen extends World
 {
+    Label avatarLabel;
     public MenuScreen() 
     {
         super(600, 400, 1);
@@ -15,7 +16,9 @@ public class MenuScreen extends World
         addObject(new Button(() -> Greenfoot.setWorld(new InstructionScreen(this))), 300, 340);
         
         AvatarManager avatarManager = new AvatarManager();
-        addObject(avatarManager,300, 100);
+        addObject(avatarManager,300, 100); 
+        avatarLabel = new Label("Choose your character!", 18);
+        addObject(avatarLabel, 300, 160);
     }
 
    
