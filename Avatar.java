0ac7229eaf8656dcca6cf610bdfa37d2863e1845
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-10-21 03:06:41 -0400

    Created Avatar Constructor

---
+++ Avatar.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Avatar here.
+ * 
+ * @author Gloriana Zheng
+ * @version Oct 2024
+ */
+public class Avatar extends Actor
+{
+    /**
+     * Act - do whatever the Avatar wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        public Avatar(String file()
+        {
+            setImage(new GreenfootImage(file));
+        }
+    }
+}
