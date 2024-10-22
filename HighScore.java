726c110ea1e898cd54a6ddf04188fa36186c03c3
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-10-21 22:38:48 -0400

    Updated High Score class

--- HighScore.java
+++ HighScore.java
@@ -12,7 +12,7 @@ public class HighScore extends World
 {
 
     private Map<String, Integer> highScores;
-    private World previousScreen;
+    
     /**
      * Constructor for objects of class HighScore.
      * 
@@ -21,5 +21,25 @@ public class HighScore extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
+        // highscore map with 3 names
+        highScores = new HashMap<>();
+        highScores.put("Mr.Chan", 2900);
+        highScores.put("Tiffany", 2800);
+        highScores.put("Andrew", 1000);
+        
+        addScores();
+    }
+    
+    private void addScores(){
+        int y = 100;
+        
+        for (Map.Entry<String, Integer> entry : highScores.entrySet()) 
+        {
+          String text = entry.getKey() + ": " + entry.getValue();
+          Label scoreLabel = new Label(text, 24);
+          addObject(scoreLabel, 300, y);
+          y+= 50;
+        }
     }
 }
